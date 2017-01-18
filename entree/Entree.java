86da6cb453c468a14bf93e80829c2406081eae3f
am163898 <mohamed.ait-mansour@etu.univ-lehavre.fr>
2017-01-18 16:14:06 +0100

    add --all

--- entree/Entree.java
+++
@@ -1,6 +0,0 @@
-import java.lang.*;
-
-public interface Entree {
-   public String toString(Presentation, Sens);
-   public boolean recherche(String);
-}
--- entree/Presentation.java
+++
@@ -1,5 +0,0 @@
-public enum Presentation {
-  ABREGE,
-  SIMPLE,
-  COMPLET;	
-}
