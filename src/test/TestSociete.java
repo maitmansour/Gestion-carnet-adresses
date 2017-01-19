9926812bc875c2c16ed21a92ceebd14633cce9b9
maitmansour <mohamedaitmansour95@gmail.com>
2017-01-19 22:17:04 +0100

    Etape 8

---
+++ src/test/TestSociete.java
@@ -0,0 +1,13 @@
+package test;
+
+import entree.Societe;
+
+public class TestSociete {
+
+	public static void main(String[] args) {
+		Societe societe = new Societe("Ecole de sorcellerie Poudlard");
+		System.out.println("Test Societé : "+societe.toString());
+
+	}
+
+}
