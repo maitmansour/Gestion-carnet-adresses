48fcbb60b69d92dbb3f195ec2417196a931a5f31
maitmansour <mohamedaitmansour95@gmail.com>
2017-01-19 21:38:09 +0100

    Etape 6 - Societé à voir, si la chaine - Société : xxxx ...

--- src/entree/Societe.java
+++ src/entree/Societe.java
@@ -1,6 +1,21 @@
 package entree;
 
-public class Societe {
+public class Societe implements Entree{
 	private String raisonSociale;
+
+	public Societe(String raisonSociale) {
+		this.raisonSociale = raisonSociale;
+	}
+
+	@Override
+	public String toString() {
+		return raisonSociale;
+	}
+
+	@Override
+	public boolean recherche() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
