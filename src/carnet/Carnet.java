f937e0d768e6e9cf4384e62ef47375d3298141a4
Mohamed AIT MANSOUR <mohamed.ait-mansour@etu.univ-lehavre.fr>
2017-01-20 12:42:30 +0100

    Correction Getters/Setters And Developping Lecture

--- src/carnet/Carnet.java
+++ src/carnet/Carnet.java
@@ -3,6 +3,7 @@ package carnet;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import entree.*;
 
@@ -11,6 +12,11 @@ public class Carnet {
     private Entree selectionnees[];
 	public Entree[] getEntrees() {
 		return entrees;
+	}
+	public Carnet() {
+		super();
+		this.entrees = new Entree[10];
+		this.selectionnees = new Entree[10];
 	}
 	public void setEntrees(Entree[] entrees) {
 		this.entrees = entrees;
@@ -27,21 +33,35 @@ public class Carnet {
 			//FORMAT : ID;TYPE;CHAMP1;CHAMP2;CHAMP3
 
 			while ((sCurrentLine = br.readLine()) != null) {
-					System.out.println(sCurrentLine);
+				//	System.out.println(sCurrentLine);
 					String[] lines = sCurrentLine.split(";");
 					switch (lines[1]) {
 					case "SOCIETE":
+						entrees[1]= new Societe(lines[2]);
 						break;
 
 					case "PERSONNE":
 						String[] prenoms = lines[2].split(",");
 						for (String prenom : prenoms) {
-							System.out.println(prenom);
+						//	System.out.println(prenom);
 						}
+						//Id personnes ??
+						//Tableau de type ?
+						Genre genre;
+						if (lines[4].equalsIgnoreCase("H")) {
+							genre=Genre.HOMME;
+						}else{
+							genre=Genre.FEMME;
+
+						}
+						entrees[0] = new Personne(lines[3], prenoms, genre, /*lines[5]*/null, new Societe(lines[6]), lines[7]);
 						break;
 					}
 
 			}
+			
+			
+			
 			
 
 
@@ -49,6 +69,14 @@ public class Carnet {
 			e.printStackTrace();
 		}
 
+	}
+	
+	@Override
+	public String toString() {
+		//return "Carnet [entrees=" + Arrays.toString(entrees) + ", selectionnees=" + Arrays.toString(selectionnees)
+		//		+ "]";
+		
+	return "Carnet [entrees=" + Arrays.toString(entrees)+ "]";
 	}
 	
 }
--- src/entree/Societe.java
+++ src/entree/Societe.java
@@ -3,14 +3,22 @@ package entree;
 public class Societe implements Entree{
 	private String raisonSociale;
 
-	public Societe(String raisonSociale) {
+	public String getRaisonSociale() {
+		return raisonSociale;
+	}
+
+	public void setRaisonSociale(String raisonSociale) {
 		this.raisonSociale = raisonSociale;
+	}
+
+	public Societe(String raisonSociale) {
+		setRaisonSociale(raisonSociale);
 	}
 
 	@Override
 	public String toString() {
-		return raisonSociale;
-	}
+		return 	getRaisonSociale();
+		}
 
 	@Override
 	public boolean recherche() {
