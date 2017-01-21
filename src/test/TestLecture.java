c86e246b0ba4061fdbc7cf7d5f1c1a674017dda2
bm163251 <belgharbimeryem@gmail.com>
2017-01-21 13:50:22 +0100

    TO DO : Recherche() & Test si déja existe

--- src/test/TestLecture.java
+++ src/test/TestLecture.java
@@ -1,4 +1,7 @@
 package test;
+import java.util.ArrayList;
+import java.util.List;
+
 import carnet.*;
 import entree.*;
 
@@ -7,9 +10,28 @@ public class TestLecture {
 	public static void main(String[] args) {
 		Carnet carnet = new Carnet();
 		carnet.lectureFichier("C:\\Users\\user\\Downloads\\filename.txt");
-		carnet.affichage(Ordre.DECROISSANT, Presentation.ABREGE, Sens.NOM_PRENOMS);
-		//carnet.affichageSelection(Ordre.DECROISSANT, Presentation.ABREGE, Sens.NOM_PRENOMS);
+		//Test Ajouter entree
+		Societe societe = new Societe("TEST TEST AJOUT SOCIETE");
+		String prenoms[]= new String[3];
+		prenoms[0]="Jhone";
+		prenoms[1]="Deuxieme";
+		prenoms[2]="Troisieme";
+		Personne personne = new Personne("AJOUT PERS", prenoms, Genre.HOMME, null, societe, "Directeur");
+		
+		//carnet.selection(personne);
+		//carnet.selection(societe);
 
+		//carnet.affichage(Ordre.DECROISSANT, Presentation.ABREGE, Sens.NOM_PRENOMS);
+		//carnet.affichageSelection(Ordre.DECROISSANT, Presentation.ABREGE, Sens.NOM_PRENOMS);
+		
+		
+		List<Entree>  tmpArray = new ArrayList<Entree>();
+			tmpArray.add(personne);
+			tmpArray.add(societe);
+		carnet.selection(tmpArray);
+		//carnet.deselection();
+		carnet.affichageSelection(Ordre.DECROISSANT, Presentation.ABREGE, Sens.NOM_PRENOMS);
+		
 	}
 	
 
